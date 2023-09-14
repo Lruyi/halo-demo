7908905893b9fcb6b6f4a0bf620b171a3ea55281
liuruyi <liuruyi2@tal.com>
2023-09-14 18:51:18 +0800

    修改配置

--- src/main/java/com/halo/demo/HaloDemoApplication.java
+++ src/main/java/com/halo/demo/HaloDemoApplication.java
@@ -8,6 +8,7 @@ public class HaloDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HaloDemoApplication.class, args);
+        System.out.println("HaloDemoApplication START SUCCESS!");
     }
 
 }
