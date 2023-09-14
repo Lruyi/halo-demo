56863d640db38e87627864643a91cfad43c442bf
liuruyi <liuruyi2@tal.com>
2023-09-14 18:39:53 +0800

    Initial commit

---
+++ src/main/java/com/halo/demo/HaloDemoApplication.java
@@ -0,0 +1,13 @@
+package com.halo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaloDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/halo/demo/HaloDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaloDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
